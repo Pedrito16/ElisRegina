8e40467073017aa18cfeafdfdb8463814250d4a1
Pedrito16 <pedrogabrielvicente98@gmail.com>
2024-05-27 10:52:00 -0300

    adicionei tijolos quebraveis
    tijolo usado para construção de cenários

--- elisRegina/Assets/Scripts/Scripts do player/Peso.cs
+++ elisRegina/Assets/Scripts/Scripts do player/Peso.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class Peso : MonoBehaviour
 {
+    public int damage = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,7 +29,7 @@ public class Peso : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
 
-            Destroy(gameObject, 2f);
+            Destroy(gameObject, 1f);
 
         }
     }
