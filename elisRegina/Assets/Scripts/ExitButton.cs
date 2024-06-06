0109ddec877487382835fe635eb13c11f0a240e0
Ventu0 <matheus.ventura2909@gmail.com>
2024-06-06 16:39:15 -0300

    tentativa de saida

--- elisRegina/Assets/Scripts/ExitButton.cs
+++ elisRegina/Assets/Scripts/ExitButton.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
 
 public class ExitButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+
         
+
+    }
+   public void Quitgame()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
