64a1c029cfbc4cbf80c743c69c4dcf137abad683
Pedrito16 <pedrogabrielvicente98@gmail.com>
2024-06-11 16:39:53 -0300

    Programei a animação, mas surgiu um bug

--- elisRegina/Assets/Scripts/Scripts do player/Player.cs
+++ elisRegina/Assets/Scripts/Scripts do player/Player.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int moveSpeed = 5;
-    float horizontal;
+    public float moveSpeed = 5;
+    float horizontal = 1;
     public int score;
     public float jumpStrenght = 5;
     public Rigidbody2D body;
@@ -16,18 +16,27 @@ public class Player : MonoBehaviour
     public Transform Direita, Esquerda;
     public float fireRate = 0.5f;
     public float nextFire = 0.5f;
-
+    public Animator animator;
+    public Transform playerTransform;
     void Start()
     {
+        //playerTransform.localScale = new Vector2(direction, 1);
         
     }
 
     void Update()
     {
+        Vector2 scale = playerTransform.localScale;
+        scale.x = direction;
+        playerTransform.localScale = scale;
+
         horizontal = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2( horizontal * moveSpeed, body.velocity.y );
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
         groundCheck = Physics2D.OverlapCircle(foot.position, 0.05f);
+        
 
         if(Input.GetButtonDown("Jump")&&  groundCheck)
         {
@@ -40,12 +49,12 @@ public class Player : MonoBehaviour
             direction = (int)horizontal;
         }
         
-        if (Input.GetButtonDown("Weight") && direction == 1 && Time.time > nextFire)
+        if (Input.GetButtonDown("Weight") && direction == 1) //1 && Time.time > nextFire
         {
          Instantiate(Peso, Direita.position, transform.rotation);
 
         }
-        else if (Input.GetButtonDown("Weight")&& direction == -1)
+        else if (Input.GetButtonDown("Weight") && direction == -1)
         {
             Instantiate(Peso, Esquerda.position, transform.rotation);
 
