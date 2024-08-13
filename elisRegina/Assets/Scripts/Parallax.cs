c508e4466b54ff03efb06f622921a217a5616de7
Pedrito16 <pedrogabrielvicente98@gmail.com>
2024-08-13 16:36:39 -0300

    fazendo parallasx

---
+++ elisRegina/Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPosition;
+    public GameObject cam;
+    public float efeitoParallax;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
