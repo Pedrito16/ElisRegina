fd9e4bff78e5a7225a6a618463c8d55cf17ab36b
Pedrito16 <pedrogabrielvicente98@gmail.com>
2024-09-11 10:06:32 -0300

    fazendo cutscene

---
+++ elisRegina/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField] Animator startAnimation;
+    [SerializeField] string parameterName;
+    void Start()
+    {
+        StartCoroutine(FadeOutCooldown());
+    }
+    IEnumerator FadeOutCooldown()
+    {
+        startAnimation.SetBool(parameterName, false);
+        yield return new WaitForSeconds(1f);
+        startAnimation.SetBool(parameterName, true);
+    }
+}
