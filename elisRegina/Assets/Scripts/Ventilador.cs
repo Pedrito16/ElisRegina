f7d381cb34594ef18459a523bdb144506a7c68d5
Pedrito16 <pedrogabrielvicente98@gmail.com>
2024-09-17 14:13:29 -0300

    terminei o ventilador PORRAAAA
    2 dias fazendo essa disgraça

--- elisRegina/Assets/Scripts/Ventilador.cs
+++ elisRegina/Assets/Scripts/Ventilador.cs
@@ -3,41 +3,82 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 public class Ventilador : MonoBehaviour
 {
     public float velocitySpeed;
+    
     [SerializeField] GameObject ventoBaixoObject, ventoCimaObject;
     public ParticleSystem ventoBaixo, ventoCima;
     public Alavanca alavancaScript;
     [SerializeField] Animator ventiladorAnimator;
     [SerializeField] bool inverter = false; //serve para inverter os sentidos
+    private void Start()
+    {
+        if (alavancaScript.currentState)
+        {
+            ventoBaixo.Play();
+        }
+        else
+        {
+            ventoCima.Play();
+        }
+    }
     void Update()
     {
-        ventiladorAnimator.SetBool("Trocar", !alavancaScript.currentState);
+        if(!inverter) 
+        {
+            ventiladorAnimator.SetBool("Trocar", !alavancaScript.currentState);
+        }
+        else
+        {
+            ventiladorAnimator.SetBool("Trocar", alavancaScript.currentState);
+        }
+        
         if (alavancaScript.currentState == true && inverter == false)
         {
-            ventoCimaObject.SetActive(false);
-            ventoBaixoObject.SetActive(true);
-            ventoBaixo.Play();
+            if (ventoCima.isPlaying)
+            {
+                ventoCima.Stop();
+            }
+            if (!ventoBaixo.isPlaying)
+            {
+                ventoBaixo.Play();
+            }
         }
         else if(alavancaScript.currentState == true && inverter == true) 
         {
-            ventoCimaObject.SetActive(true);
-            ventoBaixoObject.SetActive(false);
-            ventoCima.Play();
+            if (ventoBaixo.isPlaying)
+            {
+                ventoBaixo.Stop();
+            }
+            if (!ventoCima.isPlaying)
+            {
+                ventoCima.Play();
+            }
         }
-        if(alavancaScript.currentState == false && inverter == false) 
+        if(alavancaScript.currentState == false && inverter == false)
         {
-            ventoCimaObject.SetActive(true);
-            ventoBaixoObject.SetActive(false);
-            ventoCima.Play();
+            if (ventoBaixo.isPlaying)
+            {
+                ventoBaixo.Stop();
+            }
+            if (!ventoCima.isPlaying)
+            {
+                ventoCima.Play();
+            }
         }
         else if(alavancaScript.currentState == false && inverter == true) //se o estado atual for verde
         {
-            ventoCimaObject.SetActive(false);
-            ventoBaixoObject.SetActive(true);
-            ventoBaixo.Play();
+            if (ventoCima.isPlaying)
+            {
+                ventoCima.Stop();
+            }
+            if (!ventoBaixo.isPlaying)
+            {
+                ventoBaixo.Play();
+            }
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
@@ -60,13 +101,13 @@ public class Ventilador : MonoBehaviour
             if(inverter == true && alavancaScript.currentState == true)
             {
                 Vector2 newYvelocity = rb.velocity;
-                newYvelocity.y = velocitySpeed * -1;
+                newYvelocity.y = velocitySpeed;
                 rb.velocity = newYvelocity;
             }
             else if(inverter == true && alavancaScript.currentState == false)
             {
                 Vector2 newYvelocity = rb.velocity;
-                newYvelocity.y = velocitySpeed;
+                newYvelocity.y = velocitySpeed * -1;
                 rb.velocity = newYvelocity;
             }
         }
